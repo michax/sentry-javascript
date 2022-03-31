723c3c20f69b01aace744dad40006b79cdffcdcb
Lukas Stracke <lukas.stracke@sentry.io>
2022-03-31 12:14:25 +0200

    ref(build): introduce central `build` directory to packages with bundles
    in this commit:
    * move browser npm build files to `build/npm`
    * adjust `postbuild.ts` for tmp copying of bundles into npm

--- scripts/postbuild.ts
+++ scripts/postbuild.ts
@@ -6,16 +6,16 @@
 */
 
 import * as fs from 'fs';
-
+import * as fse from 'fs-extra';
 import * as path from 'path';
 
-const BUILD_DIR = 'build';
+const NPM_BUILD_DIR = 'build/npm';
 const ASSETS = ['README.md', 'LICENSE', 'package.json', '.npmignore'];
 const ENTRY_POINTS = ['main', 'module', 'types'];
 
-// check if build dir exists
-if (!fs.existsSync(path.resolve(BUILD_DIR))) {
-  console.error(`Directory ${BUILD_DIR} DOES NOT exist`);
+// check if build dirs exists
+if (!fs.existsSync(path.resolve(NPM_BUILD_DIR))) {
+  console.error(`Directory ${NPM_BUILD_DIR} DOES NOT exist`);
   console.error("This script should only be executed after you've run `yarn build`.");
   process.exit(1);
 }
@@ -27,21 +27,33 @@ ASSETS.forEach(asset => {
     console.error(`Asset ${asset} does not exist.`);
     process.exit(1);
   }
-  fs.copyFileSync(assetPath, path.resolve(BUILD_DIR, asset));
+  fs.copyFileSync(asset, path.resolve(NPM_BUILD_DIR, asset));
 });
 
+// TODO remove in v7! Until then:
+// copy CDN bundles into npm dir to temporarily keep bundles in npm tarball
+// inside the tarball, they are located in `build/`
+const npmTmpBundlesPath = path.resolve(NPM_BUILD_DIR, 'build');
+const cdnBundlesPaht = path.resolve('build', 'bundles');
+if (!fs.existsSync(npmTmpBundlesPath)) {
+  fs.mkdirSync(npmTmpBundlesPath);
+}
+fse.copy(cdnBundlesPaht, npmTmpBundlesPath);
+// end remove
+
 // package.json modifications
-const packageJsonPath = path.resolve(BUILD_DIR, 'package.json');
+const packageJsonPath = path.resolve(NPM_BUILD_DIR, 'package.json');
 const pkgJson: { [key: string]: string } = require(packageJsonPath);
 
 // modify entry points to point to correct paths (i.e. strip out the build directory)
 ENTRY_POINTS.filter(entryPoint => pkgJson[entryPoint]).forEach(entryPoint => {
-  pkgJson[entryPoint] = pkgJson[entryPoint].replace(`${BUILD_DIR}/`, '');
+  pkgJson[entryPoint] = pkgJson[entryPoint].replace(`${NPM_BUILD_DIR}/`, '');
 });
 
 // TODO decide if we want this:
 delete pkgJson.scripts;
 delete pkgJson.volta;
+delete pkgJson.jest;
 
 // write modified package.json to file (pretty-printed with 2 spaces)
 fs.writeFileSync(packageJsonPath, JSON.stringify(pkgJson, null, 2));
